b4f149a819c8718d01855c4cc5c9ec96d3d77cff
Heang_YG <yoeurngheang33@gmail.com>
2022-12-24 11:56:33 +0700

    clothigshop PHP

---
+++ footer.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- css custom cdn link -->
+    <link rel="stylesheet" href="custom.css">
+    <title>Footer</Footer></title>
+</head>
+<body>
+<footer style="background-color:rgb(237, 237, 237)">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">Company</h5>
+            <ul class="list-unstyled mb-0">
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">frequently asked questions</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">delivery</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">pricing</a>
+                </li>
+                <li>
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">where we deliver?</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+            <h5 class="mb-3 " style="letter-spacing: 2px; color: #7f4722;">Information</h5>
+            <ul class="list-unstyled mb-0">
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f;text-decoration:none;">frequently asked questions</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">delivery and payment</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">activate the card</a>
+                </li>
+                <li>
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">rules</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+            <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722">Service</h5>
+            <ul class="list-unstyled mb-0">
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">buy a gift card</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">history</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">return</a>
+                </li>
+                <li>
+                    <a href="#!" style="color: #4f4f4f;text-decoration:none;">contact</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+            <h5 class="mb-3" style="letter-spacing: 2px; color: #7f4722;">Contact</h5>
+            <ul class="list-unstyled mb-0">
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f;text-decoration:none;">registration</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f;text-decoration:none;">application</a>
+                </li>
+                <li class="mb-1">
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">about program</a>
+                </li>
+                <li>
+                    <a href="#!" style="color: #4f4f4f; text-decoration:none;">rules</a>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+    <div class="buttom text-center p-3" style="background-color: rgb(237, 237, 237);">
+        <label for="#">© 2022 Copyright:</label>
+        <a class="text-dark" href="#">HELLO</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- fontawsome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    
+    <!-- css custom cdn link -->
+    <link rel="stylesheet" href="custom.css">
+
+    <title>Header</title>
+</head>
+<body>
+    <div class="header sticky-top">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="logo">
+                <a class="navbar-brand" href="#">HELLO</a>
+            </div>
+            <div class="nav-start">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="">
+                        <a class="nav-link" href="#">shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">about</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-end">
+                <a href="#"><i class="fa-regular fa-user"></i></a>
+                <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                <a href="#"><i class="fa-regular fa-star"></i></a>
+                <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+            </div>
+        </nav>
+
+        
+    </div>
+</body>
+</html>
